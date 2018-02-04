44d577e526db9ec5b773fc3edaea845dcd287e52
gerardpf2 <gerard.contacte@gmail.com>
2018-02-04 12:16:54 +0100

    Cleaned ModuleRender

--- KeepCryingEngine/ModuleRender.h
+++ KeepCryingEngine/ModuleRender.h
@@ -9,20 +9,11 @@
 
 #include "Module.h"
 
-struct ILinfo;
-
-struct Vertex
-{
-	GLfloat position[3];
-	GLfloat color[4];
-	GLfloat uv[2];
-};
-
 class ModuleRender : public Module
 {
 public:
 	ModuleRender();
-	~ModuleRender();
+	virtual ~ModuleRender();
 
 	bool Init() override;
 	update_status PreUpdate(float deltaTimeS, float realDeltaTimeS) override;
@@ -32,100 +23,16 @@ public:
 
 	void DrawCross(const float3& pos, float scale) const;
 
-	int getImageWidth() const;
-	int getImageHeight() const;
-	int getImageFormat() const;
-	int getImageSizeBytes() const;
-	int getImagePixelDepth() const;
-
-	uint getWrapModeS() const;
-	void setWrapModeS(uint wrapModeS);
-
-	uint getWrapModeT() const;
-	void setWrapModeT(uint wrapModeT);
-
-	bool getMagFilter() const;
-	void setMagFilter(bool magFilter);
-
-	bool getMinFilter() const;
-	void setMinFilter(bool minFilter);
-
-	uint getMagFilterMode() const;
-	void setMagFilterMode(uint magFilterMode);
-
-	uint getMinFilterMode() const;
-	void setMinFilterMode(uint minFilterMode);
-
-	bool getMipmap() const;
-	void setMipmap(bool mipMap);
-
-	bool getAnisotropicFilter() const;
-	void setAnisotropicFilter(bool anisotropicFilter);
-
-	void SetCurrentTexture(uint textureIndex);
-	uint LoadTexture(const char* theFileName, ILinfo* imageInfo);
-
 private:
-	// void SetUpShaderStruct() const;
-	void FillVerticesData(uint n, const float* positions, const float* colors, const float* texCoords, Vertex* vertices) const;
-
-	void SetUpTextures();
-	void CleanUpTextures();
-	void SetUpTexture();
-	void SetUpBigArray(float3 pos,float size);
-	void SetUpIndicesArray(float3 pos, float size);
-	void SetUpSphere(float3 pos,float radius, unsigned int rings, unsigned int sectors);
-	void SetUpPlane(float3 pos, float size);
-	void DrawCubeDirect(float3 pos, float size) const;
-	void DrawCubeBigArray() const;
-	void DrawCubeIndices() const;
-	void DrawSphere() const;
-	void DrawPlane() const;
-
-	void SetUpGrid() const;
 	void DrawGrid() const;
 
-	void SetUpLight();
+	void SetUpLight() const;
 
 public:
-	uint* actualTexture = nullptr;
-	uint debugTexture = 0;
-	uint lenaTexture = 0;
-	uint exodiaTexture = 0;
-	uint rockTexture = 0;
-
 	GLfloat globalAmbient[4] = { 0.2f, 0.2f, 0.2f, 1.0f };
 
 private:	
-	uint bigArrayCube = 0;
-	uint vertexArrayBuffer = 0;
-	uint indicesArrayBuffer = 0;
-	// uint uvArrayBuffer = 0;
-	uint sphereIndex = 0;
-	uint sphereVertex = 0;
-	// uint sphereUV = 0;
-	uint planeVertices = 0;
-	uint planeUVs = 0;
-
-	int sphereIndicesSize = 0;
-	static const uint CHECKERS_HEIGHT;
-	static const uint CHECKERS_WIDTH;
 	SDL_GLContext glcontext = nullptr;
-
-	ILinfo* currentImageInfo = nullptr;
-
-	ILinfo* lenaImageInfo = nullptr;
-	ILinfo* rockImageInfo = nullptr;
-	ILinfo* exodiaImageInfo = nullptr;
-
-	uint wrapModeS = 0;
-	uint wrapModeT = 0;
-	bool magFilter = true;
-	bool minFilter = true;
-	uint magFilterMode = 0;
-	uint minFilterMode = 0;
-	bool mipmap = false;
-	bool anisotropicFilter = false;
 };
 
 #endif // !_MODULERENDER_H_
