8f5a1bee365cee3e2636e13be0c51200f63a51dd
Pere Viader <pere.viader22@gmail.com>
2018-02-07 22:48:38 +0100

    Added world and local functionality to transform

--- KeepCryingEngine/Transform.cpp
+++ KeepCryingEngine/Transform.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-Transform::Transform() :Component(ComponentType::Transform), position(float3::zero), rotation(Quat::identity), scale(float3::one)
+Transform::Transform() :Component(ComponentType::Transform), localPosition(float3::zero), localRotation(Quat::identity), localScale(float3::one)
 { }
 
 Transform::~Transform()
@@ -16,60 +16,176 @@ void Transform::DrawUI()
 {
 	if(ImGui::CollapsingHeader("Transform"))
 	{
-		ImGui::DragFloat3(" Position", position.ptr(),0.1f);
-		float3 angles = RadToDeg(rotation.ToEulerXYZ());
+		ImGui::DragFloat3(" Position", localPosition.ptr(),0.1f);
+		float3 angles = RadToDeg(localRotation.ToEulerXYZ());
 		if(ImGui::DragFloat3(" Rotation", angles.ptr()), 0.1f)
 		{
 			angles = DegToRad(angles);
-			rotation = Quat::FromEulerXYZ(angles.x, angles.y, angles.z);
+			localRotation = Quat::FromEulerXYZ(angles.x, angles.y, angles.z);
 		}
-		ImGui::DragFloat3(" Scale", scale.ptr(), 0.1f);
+		ImGui::DragFloat3(" Scale", localScale.ptr(), 0.1f);
 	}
 }
 
-float4x4 Transform::GetAcumulatedTransform()
-{
-	if(gameObject->GetParent())
-	{
-		Transform* parent = (Transform*)gameObject->GetParent()->GetComponent(ComponentType::Transform);
-		float4x4 parentTransformMatrix = parent->GetAcumulatedTransform();
-		return parentTransformMatrix * float4x4::FromTRS(position, rotation.ToFloat4x4(), scale);
-	}
-	else 
-	{
-		return float4x4::identity;
-	}
-}
-
-float3x3 Transform::GetAcumulatedRotation()
-{
-	if(gameObject->GetParent())
-	{
-		Transform* parent = (Transform*)gameObject->GetParent()->GetComponent(ComponentType::Transform);
-		float3x3 parentTransformMatrix = parent->GetAcumulatedRotation();
-		return parentTransformMatrix * rotation; // float3x3::FromTRS(float3::zero, rotation.ToFloat4x4(), float3::one);
-	}
-	else
-	{
-		return float3x3::identity;
-	}
-}
-
-float4x4 Transform::GetAcumulatedTransformWithoutScale()
-{
-	if(gameObject->GetParent())
-	{
-		Transform* parent = (Transform*)gameObject->GetParent()->GetComponent(ComponentType::Transform);
-		float4x4 parentTransformMatrix = parent->GetAcumulatedTransformWithoutScale();
-		return parentTransformMatrix * float4x4::FromTRS(position, rotation.ToFloat4x4(), float3::one);
-	}
-	else
-	{
-		return float4x4::identity;
-	}
-}
+//
+//float4x4 Transform::GetAcumulatedTransform()
+//{
+//	if(gameObject->GetParent())
+//	{
+//		Transform* parent = (Transform*)gameObject->GetParent()->GetComponent(ComponentType::Transform);
+//		float4x4 parentTransformMatrix = parent->GetAcumulatedTransform();
+//		return parentTransformMatrix * float4x4::FromTRS(localPosition, localRotation.ToFloat4x4(), localScale);
+//	}
+//	else 
+//	{
+//		return float4x4::identity;
+//	}
+//}
+//
+//float3x3 Transform::GetAcumulatedRotation()
+//{
+//	if(gameObject->GetParent())
+//	{
+//		Transform* parent = (Transform*)gameObject->GetParent()->GetComponent(ComponentType::Transform);
+//		float3x3 parentTransformMatrix = parent->GetAcumulatedRotation();
+//		return parentTransformMatrix * localRotation; // float3x3::FromTRS(float3::zero, rotation.ToFloat4x4(), float3::one);
+//	}
+//	else
+//	{
+//		return float3x3::identity;
+//	}
+//}
+//
+//float4x4 Transform::GetAcumulatedTransformWithoutScale()
+//{
+//	if(gameObject->GetParent())
+//	{
+//		Transform* parent = (Transform*)gameObject->GetParent()->GetComponent(ComponentType::Transform);
+//		float4x4 parentTransformMatrix = parent->GetAcumulatedTransformWithoutScale();
+//		return parentTransformMatrix * float4x4::FromTRS(localPosition, localRotation.ToFloat4x4(), float3::one);
+//	}
+//	else
+//	{
+//		return float4x4::identity;
+//	}
+//}
 
 vector<ComponentType> Transform::GetProhibitedComponents() const
 {
 	return { ComponentType::Transform };
+}
+
+const float3 & Transform::GetLocalPosition() const
+{
+	return localPosition;
+}
+
+const Quat & Transform::GetLocalRotation() const
+{
+	return localRotation;
+}
+
+const float3 & Transform::GetLocalScale() const
+{
+	return localScale;
+}
+
+void Transform::SetLocalPosition(const float3 & position)
+{
+	dirty = true;
+	localPosition = position;
+}
+
+void Transform::SetLocalRotation(const Quat & rotation)
+{
+	dirty = true;
+	localRotation = rotation;
+}
+
+void Transform::SetLocalScale(const float3 & scale)
+{
+	dirty = true;
+	localScale = scale;
+}
+
+const float3 & Transform::GetWorldPosition() const
+{
+	RecalculateIfNecessary();
+	return worldPosition;
+}
+
+const Quat & Transform::GetWorldRotation() const
+{
+	RecalculateIfNecessary();
+	return worldRotation;
+}
+
+const float3 & Transform::GetWorldScale() const
+{
+	RecalculateIfNecessary();
+	return worldScale;
+}
+
+/*
+	Set local variables instead of world ones
+	Then we calculate the world variables from the local ones when they are asked for
+*/
+void Transform::SetWorldPosition(const float3 & position)
+{
+	dirty = true;
+	float3 worldPositionDelta = position - worldPosition;
+	localPosition = localPosition + worldPositionDelta;
+}
+
+void Transform::SetWorldRotation(const Quat & rotation)
+{
+	dirty = true;
+	Quat worldRotationDelta = rotation.Mul(worldRotation.Inverted());
+	worldRotation = localRotation.Mul(worldRotationDelta);
+}
+
+void Transform::SetWorldScale(const float3 & scale)
+{
+	dirty = true;
+	float3 worldScaleDelta = scale - worldScale;
+	localScale = localScale + worldScaleDelta;
+}
+
+const float4x4 & Transform::GetModelMatrix() const
+{
+	RecalculateIfNecessary();
+	return modelMatrix;
+}
+
+float4x4 Transform::GetLocalMatrix() const
+{
+	return float4x4::FromTRS(localPosition, localRotation.ToFloat4x4(), localScale);
+}
+
+float4x4 Transform::GetParentMatrix() const
+{
+	float4x4 parentMatrix = float4x4::identity;
+	GameObject* parent = gameObject->GetParent();
+	if (parent)
+	{
+		Transform* parentTransform = (Transform*)parent->GetComponent(ComponentType::Transform);
+		parentMatrix = parentTransform->GetModelMatrix();
+	}
+	return parentMatrix;
+}
+
+/*
+	Const function that changes mutable data
+	It needs to be const because it must update the matrix only when necessary
+*/
+void Transform::RecalculateIfNecessary() const
+{
+	if (dirty) 
+	{
+		float4x4 localMatrix = GetLocalMatrix();
+		float4x4 parentMatrix = GetParentMatrix();
+		modelMatrix = parentMatrix * localMatrix;
+		modelMatrix.Decompose(worldPosition, worldRotation, worldScale);
+		dirty = false;
+	}
 }
--- KeepCryingEngine/Transform.h
+++ KeepCryingEngine/Transform.h
@@ -14,17 +14,47 @@ public:
 	virtual ~Transform();
 
 	void DrawUI() override;
-	float4x4 GetAcumulatedTransform();
-	
-	float3x3 GetAcumulatedRotation();
-	float4x4 GetAcumulatedTransformWithoutScale();
+
+	//float4x4 GetAcumulatedTransform();
+	//
+	//float3x3 GetAcumulatedRotation();
+	//float4x4 GetAcumulatedTransformWithoutScale();
 
 	virtual std::vector<ComponentType> GetProhibitedComponents() const override;
 
-public:
-	float3 position;
-	Quat rotation;
-	float3 scale;
+	const float3& GetLocalPosition() const;
+	const Quat& GetLocalRotation() const;
+	const float3& GetLocalScale() const;
+
+	void SetLocalPosition(const float3& position);
+	void SetLocalRotation(const Quat& rotation);
+	void SetLocalScale(const float3& scale);
+
+	const float3& GetWorldPosition() const;
+	const Quat& GetWorldRotation() const;
+	const float3& GetWorldScale() const;
+
+	void SetWorldPosition(const float3& position);
+	void SetWorldRotation(const Quat& rotation);
+	void SetWorldScale(const float3& scale);
+
+	const float4x4& GetModelMatrix() const;
+
+private:
+	float4x4 GetLocalMatrix() const;
+	float4x4 GetParentMatrix() const;
+	void RecalculateIfNecessary() const;
+
+private:
+	float3 localPosition;
+	Quat localRotation;
+	float3 localScale;
+
+	mutable bool dirty;
+	mutable float4x4 modelMatrix;
+	mutable float3 worldPosition;
+	mutable Quat worldRotation;
+	mutable float3 worldScale;
 };
 
 #endif // !_TRANSFORM_H_
