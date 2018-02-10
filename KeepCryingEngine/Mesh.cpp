59e29600d61c55246d3d20c6c28b7539905aa8ac
Pere Viader <pere.viader22@gmail.com>
2018-02-10 22:37:39 +0100

    Added previous mesh file

--- KeepCryingEngine/Mesh.cpp
+++ KeepCryingEngine/Mesh.cpp
@@ -1,22 +1,12 @@
 #include "Mesh.h"
 
-#define _USE_MATH_DEFINES
-
-#include <math.h>
 #include <vector>
 
-#include <MathGeoLib.h>
-
-#include "GameObject.h"
-#include "Application.h"
-#include "ModuleRender.h"
-#include "Transform.h"
-#include "ModuleCamera.h"
-#include "Camera.h"
+#include "Globals.h"
 
 using namespace std;
 
-Mesh::Mesh():Component(ComponentType::Mesh)
+Mesh::Mesh()
 {
 }
 
@@ -24,424 +14,75 @@ Mesh::~Mesh()
 {
 }
 
-void Mesh::RealUpdate(float deltaTimeS, float realDeltaTimeS)
+const AABB & Mesh::GetAABB() const
 {
-	if(changedMode)
-	{
-		switch(meshMode)
-		{
-			case MeshMode::CUBE:
-			{
-				SetUpCube();
-			}
-			break;
-
-			case MeshMode::SPHERE:
-			{
-				SetUpSphere();
-			}
-			break;
-		}
-		changedMode = false;
-	}
-
-	Transform* transform = (Transform*)gameObject->GetComponent(ComponentType::Transform);
-	OBB obb = originalaabb.ToOBB();
-
-	obb.Transform(transform->GetWorldRotation());
-	obb.Translate(transform->GetWorldPosition());
-	obb.Scale(transform->GetWorldPosition(), transform->GetWorldScale());
-
-    gameObject->GetAABB().SetFrom(obb);
-
-	if(debugAABB)
-	{
-		RenderAABB();
-	}
-
-	if(gameObject->GetVisible())
-	{
-		App->renderer->AddToDrawBuffer(*this);
-	}
-
-	/* Camera* enabledCamera = App->camera->GetEnabledCamera();
-
-	if(enabledCamera != nullptr && enabledCamera->IsInsideFrustum(gameObject->GetAABB()))
-	{
-		App->renderer->AddToDrawBuffer(*this);
-	} */
+	return aabb;
 }
 
-void Mesh::RenderAABB()
-{
-	float3 aabbcorners[8];
-	gameObject->GetAABB().GetCornerPoints(aabbcorners);
-
-	float currentColor[4];
-	glGetFloatv(GL_CURRENT_COLOR, currentColor);
-
-	glBegin(GL_LINES);
-	glColor4f(255.0f, 255.0f, 255.0f, 1.0f);
-
-	for(int i = 0; i < 8; i++)
-	{
-		for(int j = 0; j < 8; j++)
-		{
-			if(i != j)
-			{
-				glVertex3f(aabbcorners[i].x, aabbcorners[i].y, aabbcorners[i].z);
-				glVertex3f(aabbcorners[j].x, aabbcorners[j].y, aabbcorners[j].z);
-			}
-		}
-	}
-
-	glColor4f(currentColor[0], currentColor[1], currentColor[2], currentColor[3]);
-	glEnd();
-}
-
-void Mesh::DrawUI()
-{
-	if(ImGui::CollapsingHeader("Mesh"))
-	{
-		ImGui::Checkbox("Active", &enabled); ImGui::SameLine();
-		if(ImGui::Button("Delete Component"))
-		{
-			gameObject->RemoveComponent(this);
-		}
-
-		int tmpMeshMode = (int)meshMode;
-
-		if(ImGui::Combo("Type", &tmpMeshMode, "Cube\0Sphere"))
-		{
-			changedMode = true;
-			meshMode = (MeshMode)tmpMeshMode;
-		}
-	}
-}
-
-void Mesh::SetMeshMode(MeshMode mode)
-{
-	meshMode = mode;
-	changedMode = true;
-}
-
-vector<ComponentType> Mesh::GetNeededComponents() const
-{
-	return { ComponentType::Transform };
-}
-
-vector<ComponentType> Mesh::GetProhibitedComponents() const
-{
-	return { ComponentType::Mesh };
-}
-
-GLenum Mesh::GetDrawMode()
-{
-	return drawMode;
-}
-
-uint Mesh::GetVertexBufferId() const
+GLuint Mesh::GetVertexBufferId() const
 {
 	return vertexBufferId;
 }
 
-uint Mesh::GetIndicesBufferId() const
+GLuint Mesh::GetIndicesBufferId() const
 {
 	return indicesBufferId;
 }
 
-uint Mesh::GetNormalBufferId() const
+GLsizei Mesh::GetVerticesNumber() const
 {
-	return normalBufferId;
+	return nVertices;
 }
 
-uint Mesh::GetVerticesNumber() const
+GLsizei Mesh::GetIndicesNumber() const
 {
-	return verticesNumber;
+	return nIndices;
 }
 
-void Mesh::SetUpCube()
+GLenum Mesh::GetDrawMode() const
 {
-	drawMode = GL_TRIANGLES;
+	return drawMode;
+}
 
-	float size = 1.0f;
+void Mesh::SetMeshData(const vector<Vertex>& vertices, const vector<GLushort>& indices, GLenum drawMode)
+{
+	GenerateBuffers(vertices, indices);
+	CalculateAABBForMesh(vertices);
+	this->drawMode = drawMode;
+}
 
-	const size_t nVertices = 8 * 3;
-	float uniqueVertex[nVertices*3] =
-	{
-		-size, -size, -size, // 0 Front bottom left
-		size, -size, -size,  // 1 Front bottom right
-		size, size, -size,  // 2 Front top right
-		-size, size, -size, // 3 Front top left
+void Mesh::GenerateBuffers(const vector<Vertex> vertices, const vector<GLushort> indices)
+{
+	assert(vertices.size() > 0);
+	assert(indices.size() > 0);
 
-		-size, -size, size, // 4 Left bottom left
-		-size, -size, -size, // 5 Left bottom right
-		-size, size, -size, // 6 Left top right
-		-size, size, size,  // 7 Left top left
+	nVertices = vertices.size();
+	nIndices = indices.size();
 
-		-size, size, size,  // 8 Back top left
-		size, size, size,  // 9 Back top right
-		size, -size, size,  // 10 Back bottom right
-		-size, -size, size, // 11 Back bottom left
-
-		size, -size, -size,  // 12 Right bottom left
-		size, -size, size,  // 13 Right bottom right
-		size, size, size,  // 14 Right top right
-		size, size, -size,  // 15 Right top left
-
-		-size, -size, size, // 16 Bottom top left
-		size, -size, size,  // 17 Bottom top right
-		size, -size, -size,  // 18 Bottom bottom right
-		-size, -size, -size, // 19 Bottom bottom left
-
-		-size, size, -size, // 20 Top bottom left
-		size, size, -size,  // 21 Top bottom right
-		size, size, size,  // 22 Top top right
-		-size, size, size  // 23 Top top left
-	};
-
-	CalculateAABBForMesh(uniqueVertex, nVertices);
-
-	float uv[24 * 2] =
-	{
-		0.0f, 0.0f,
-		1.0f, 0.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-
-		1.0f, 0.0f,
-		0.0f, 0.0f,
-		0.0f, 1.0f,
-		1.0f, 1.0f,
-
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-		0.0f, 0.0f,
-		1.0f, 0.0f,
-
-		0.0f, 0.0f,
-		1.0f, 0.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-
-		0.0f, 1.0f,
-		1.0f, 1.0f,
-		1.0f, 0.0f,
-		0.0f, 0.0f,
-
-		0.0f, 0.0f,
-		1.0f, 0.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f
-	};
-
-	// Color array (r, g, b, a)
-	float* colors = (float*)malloc(sizeof(float) * 24 * 4);
-	fill_n(colors, 24 * 4, 100.0f);
-
-	Vertex vertices[24];
-	FillVerticesData(24, uniqueVertex, colors, uv, vertices);
-
-	glGenBuffers(1, (GLuint*) &(vertexBufferId));
+	//Generate Vertex buffer
+	const Vertex * verticesPointer = &vertices[0];
+	glGenBuffers(1, &vertexBufferId);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 24, vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * nVertices, verticesPointer, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	GLushort indicesArray[24 * 3] =
-	{
-		3, 1, 0,
-		3, 2, 1,
 
-		7, 5, 4,
-		7, 6, 5,
-
-		11, 9, 8,
-		11, 10, 9,
-
-		15, 13, 12,
-		15, 14, 13,
-
-		19, 17, 16,
-		19, 18, 17,
-
-		23, 21, 20,
-		23, 22, 21
-	};
-	
-	verticesNumber = sizeof(indicesArray)/sizeof(uint);
-
-	glGenBuffers(1, (GLuint*) &(indicesBufferId));
+	//Generate Indices buffer
+	const GLushort* indicesPointer = &indices[0];
+	glGenBuffers(1, &indicesBufferId);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesBufferId);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * verticesNumber, indicesArray, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * nIndices, indicesPointer, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	float normal[24 * 3];
-	float offset = 3;
-	for(size_t i = 0; i < 36; i+=3)
-	{
-		int x1 = indicesArray[i] * offset;
-		int x2 = indicesArray[i + 1] * offset;
-		int x3 = indicesArray[i + 2] * offset;
-
-		int y1 = (indicesArray[i] * offset) + 1;
-		int y2 = (indicesArray[i + 1] * offset) + 1;
-		int y3 = (indicesArray[i + 2] * offset) + 1;
-
-		int z1 = (indicesArray[i] * offset) + 2;
-		int z2 = (indicesArray[i + 1] * offset) + 2;
-		int z3 = (indicesArray[i + 2] * offset) + 2;
-
-		float edge1X = uniqueVertex[x2] - uniqueVertex[x1];
-		float edge1Y = uniqueVertex[y2] - uniqueVertex[y1];
-		float edge1Z = uniqueVertex[z2] - uniqueVertex[z1];
-
-		float edge2X = uniqueVertex[x3] - uniqueVertex[x1];
-		float edge2Y = uniqueVertex[y3] - uniqueVertex[y1];
-		float edge2Z = uniqueVertex[z3] - uniqueVertex[z1];
-
-		float3 vNormal = Cross(float3(edge1X, edge1Y, edge1Z), float3(edge2X, edge2Y, edge2Z)).Normalized();
-		normal[i] = vNormal.x;
-		normal[i + 1] = vNormal.y;
-		normal[i + 2] = vNormal.z;
-	}
-
-	/* for(size_t i = 0; i < 36; i++)
-	{
-		LOG_DEBUG("%f",normal[i]);
-	} */
-
-	glGenBuffers(1, (GLuint*) &(normalBufferId));
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, normalBufferId);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * 24 * 3, normal, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	RELEASE(colors);
 }
 
-void Mesh::SetUpSphere()
+void Mesh::CalculateAABBForMesh(const vector<Vertex> &vertices)
 {
-	drawMode = GL_QUADS;
-
-	uint rings = 100;
-	uint sectors = 100;
-	uint radius = 1;
-
-	float R = 1.0f / (float)(rings - 1);
-	float S = 1.0f / (float)(sectors - 1);
-
-	vector<GLfloat> vertices;
-	vector<GLushort> indices;
-	vector<GLfloat> texcoords;
-	vector<GLfloat> normals;
-
-	uint nVertices = rings * sectors;
-
-	vertices.resize(nVertices * 3);
-	texcoords.resize(nVertices * 2);
-	normals.resize(nVertices * 3);
-
-	vector<GLfloat>::iterator v = vertices.begin();
-	vector<GLfloat>::iterator t = texcoords.begin();
-	vector<GLfloat>::iterator n = normals.begin();
-	for(unsigned int r = 0; r < rings; r++)
+	float3 * positions = new float3[vertices.size()];
+	for (size_t i = 0; i < vertices.size(); i++)
 	{
-		for(unsigned int s = 0; s < sectors; s++)
-		{
-			float y = (float)sin(-M_PI_2 + M_PI * r * R);
-			float x = (float)cos(2 * M_PI * s * S) * (float)sin(M_PI * r * R);
-			float z = (float)sin(2 * M_PI * s * S) * (float)sin(M_PI * r * R);
-
-			*t++ = s * S;
-			*t++ = r * R;
-
-			*v++ = x * radius;
-			*v++ = y * radius;
-			*v++ = z * radius;
-
-			*n++ = x;
-			*n++ = y;
-			*n++ = z;
-		}
+		positions[i] = vertices[i].position;
 	}
 
-	// Color array (r, g, b, a)
-	float* colors = (float*)malloc(sizeof(float) * nVertices * 4);
-	fill_n(colors, nVertices * 4, 100.0f);
-
-	// Cannot Vertex vVertices[nVertices];
-	Vertex* vVertices = (Vertex*)malloc(sizeof(Vertex) * nVertices * 3);
-	FillVerticesData(nVertices, vertices.data(), colors, texcoords.data(), vVertices);
-
-	CalculateAABBForMesh(vertices.data(), vertices.size()/3);
-
-	glGenBuffers(1, (GLuint*) &(vertexBufferId));
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * nVertices, vVertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glGenBuffers(1, (GLuint*) &(normalBufferId));
-	glBindBuffer(GL_ARRAY_BUFFER, normalBufferId);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * nVertices*3, ((void*)&normals[0]), GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	indices.resize(rings * sectors * 4);
-	verticesNumber = indices.size();
-
-	vector<GLushort>::iterator i = indices.begin();
-
-	for(unsigned int r = 0; r < rings - 1; r++)
-	{
-		for(unsigned int s = 0; s < sectors - 1; s++)
-		{
-			*i++ = (r + 1) * sectors + s;
-			*i++ = (r + 1) * sectors + s + 1;
-			*i++ = r * sectors + s + 1;
-			*i++ = r * sectors + s;
-		}
-	}
-
-	glGenBuffers(1, (GLuint*) &(indicesBufferId));
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesBufferId);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLshort) * verticesNumber, ((void*)&indices[0]), GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	RELEASE(colors);
-	RELEASE(vVertices);
-}
-
-void Mesh::FillVerticesData(uint n, const float* positions, const float* colors, const float* texCoords, Vertex* vertices) const
-{
-	for(uint i = 0; i < n; ++i)
-	{
-		for(uint j = 0; j < 3; ++j)
-		{
-			vertices[i].position[j] = positions[i * 3 + j];
-		}
-
-		for(uint j = 0; j < 4; ++j)
-		{
-			vertices[i].color[j] = colors[i * 4 + j];
-		}
-
-		for(uint j = 0; j < 2; ++j)
-		{
-			vertices[i].uv[j] = texCoords[i * 2 + j];
-		}
-	}
-}
-
-void Mesh::CalculateAABBForMesh(float * newVertices, size_t nVertices)
-{
-	vector<float3> vertices;
-	for(size_t i = 0; i < nVertices; i++)
-	{
-		const int currentIndex = i * 3;
-		float3 vertex(newVertices[currentIndex], newVertices[currentIndex + 1], newVertices[currentIndex + 2]);
-		vertices.push_back(vertex);
-	}
-
-	originalaabb.SetNegativeInfinity();
-	if(vertices.size() != 0)
-	{
-		originalaabb.Enclose(vertices.data(), vertices.size());
-	}
+	aabb.SetNegativeInfinity();
+	aabb.Enclose(positions, vertices.size());
+	RELEASE_ARRAY(positions);
 }
