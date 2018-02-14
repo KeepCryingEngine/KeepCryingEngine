b0aab0c15d0d5acf2ac44c4a874f48c5d301fbe8
BravoXavi <BravoXavi@users.noreply.github.com>
2018-02-14 09:51:02 +0100

    Guizmos painted on screen

--- KeepCryingEngine/ModuleUI.cpp
+++ KeepCryingEngine/ModuleUI.cpp
@@ -448,79 +448,79 @@ void ModuleUI::DrawGuizmoWindow()
 {
 	ImGui::Begin("Guizmo Window");
 
-		static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::ROTATE);
-		static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::WORLD);
-		static bool useSnap = false;
-		static float snap[3] = { 1.f, 1.f, 1.f };
+	static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
+	static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::WORLD);
+	static bool useSnap = false;
+	static float snap[3] = { 1.f, 1.f, 1.f };
 
-		if(ImGui::RadioButton("Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
-			mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
+	if(ImGui::RadioButton("Translate", mCurrentGizmoOperation == ImGuizmo::TRANSLATE))
+		mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
 
-		ImGui::SameLine();
+	ImGui::SameLine();
 
-		if(ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
-			mCurrentGizmoOperation = ImGuizmo::ROTATE;
+	if(ImGui::RadioButton("Rotate", mCurrentGizmoOperation == ImGuizmo::ROTATE))
+		mCurrentGizmoOperation = ImGuizmo::ROTATE;
 
-		ImGui::SameLine();
+	ImGui::SameLine();
 
-		if(ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
-			mCurrentGizmoOperation = ImGuizmo::SCALE;
+	if(ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
+		mCurrentGizmoOperation = ImGuizmo::SCALE;
 
-		Transform* temp = (Transform*)App->scene->Get(selectedNodeID)->GetComponent(ComponentType::Transform);
+	Transform* temp = (Transform*)App->scene->Get(selectedNodeID)->GetComponent(ComponentType::Transform);
 		
-		float matrixTranslation[3] = { temp->GetLocalPosition().x, temp->GetLocalPosition().y, temp->GetLocalPosition().z };
-		float matrixRotation[3] = { temp->GetEulerLocalRotation().x, temp->GetEulerLocalRotation().y, temp->GetEulerLocalRotation().z};
-		float matrixScale[3] = { temp->GetLocalScale().x, temp->GetLocalScale().y, temp->GetLocalScale().z};
+	float matrixTranslation[3] = { temp->GetLocalPosition().x, temp->GetLocalPosition().y, temp->GetLocalPosition().z };
+	float matrixRotation[3] = { temp->GetEulerLocalRotation().x, temp->GetEulerLocalRotation().y, temp->GetEulerLocalRotation().z};
+	float matrixScale[3] = { temp->GetLocalScale().x, temp->GetLocalScale().y, temp->GetLocalScale().z};
 		
-		//ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
+	//ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
 
-		if(ImGui::DragFloat3("Position", matrixTranslation, 0.1f))
-		{
-			temp->SetLocalPosition((float3)matrixTranslation);
-		}
-		if(ImGui::DragFloat3("Rotation", matrixRotation, 0.1f))
-		{
+	if(ImGui::DragFloat3("Position", matrixTranslation, 0.1f))
+	{
+		temp->SetLocalPosition((float3)matrixTranslation);
+	}
+	if(ImGui::DragFloat3("Rotation", matrixRotation, 0.1f))
+	{
 
-		}
-		if(ImGui::DragFloat3("Scale", matrixScale, 0.1f))
-		{
-			temp->SetLocalScale((float3)matrixScale);
-		}
+	}
+	if(ImGui::DragFloat3("Scale", matrixScale, 0.1f))
+	{
+		temp->SetLocalScale((float3)matrixScale);
+	}
 
-		float* resultantMatrix = nullptr; //TODO - This needs to be the matrix of the object
-		ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, resultantMatrix);
+	//ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, matrix);
 
-		//if(mCurrentGizmoOperation != ImGuizmo::SCALE)
-		//{
-		//	if(ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
-		//		mCurrentGizmoMode = ImGuizmo::LOCAL;
-		//	ImGui::SameLine();
-		//	if(ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
-		//		mCurrentGizmoMode = ImGuizmo::WORLD;
-		//}
-		//if(ImGui::IsKeyPressed(83))
-		//	useSnap = !useSnap;
-		//ImGui::Checkbox("", &useSnap);
-		//ImGui::SameLine();
+	if(mCurrentGizmoOperation != ImGuizmo::SCALE)
+	{
+		if(ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
+			mCurrentGizmoMode = ImGuizmo::LOCAL;
+		ImGui::SameLine();
+		if(ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD))
+			mCurrentGizmoMode = ImGuizmo::WORLD;
+	}
+	//if(ImGui::IsKeyPressed(83))
+	//	useSnap = !useSnap;
+	//ImGui::Checkbox("", &useSnap);
+	//ImGui::SameLine();
 
-		//switch(mCurrentGizmoOperation)
-		//{
-		//	case ImGuizmo::TRANSLATE:
-		//		ImGui::InputFloat3("Snap", &snap[0]);
-		//		break;
-		//	case ImGuizmo::ROTATE:
-		//		ImGui::InputFloat("Angle Snap", &snap[0]);
-		//		break;
-		//	case ImGuizmo::SCALE:
-		//		ImGui::InputFloat("Scale Snap", &snap[0]);
-		//		break;
-		//}
-		ImGuiIO& io = ImGui::GetIO();
-		ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+	//switch(mCurrentGizmoOperation)
+	//{
+	//	case ImGuizmo::TRANSLATE:
+	//		ImGui::InputFloat3("Snap", &snap[0]);
+	//		break;
+	//	case ImGuizmo::ROTATE:
+	//		ImGui::InputFloat("Angle Snap", &snap[0]);
+	//		break;
+	//	case ImGuizmo::SCALE:
+	//		ImGui::InputFloat("Scale Snap", &snap[0]);
+	//		break;
+	//}
+	ImGuiIO& io = ImGui::GetIO();
+	ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
 
-		ImGuizmo::Manipulate(App->camera->camera->GetViewMatrix().ptr(), App->camera->camera->GetProyectionMatrix().ptr(), mCurrentGizmoOperation, mCurrentGizmoMode, resultantMatrix, NULL, useSnap ? &snap[0] : NULL);
+	float* objectMatrix = (float*)temp->GetModelMatrix().ptr();
+
+	ImGuizmo::Manipulate(App->camera->camera->GetViewMatrix().ptr(), App->camera->camera->GetProyectionMatrix().ptr(), mCurrentGizmoOperation, mCurrentGizmoMode, objectMatrix, NULL, useSnap ? &snap[0] : NULL);
 	
-
 	ImGui::End();
 }
 
